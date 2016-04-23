a7433249adc385e7c576791981b24a98320995c2
R SREEHARI <sreehari.r96@gmail.com>
2016-04-23 14:14:45 +0530

    EEPROM Functions

--- Pill Dispenser 2.0/pill2.c
+++ Pill Dispenser 2.0/pill2.c
@@ -973,3 +973,118 @@ void Get_RTC_Date(void)
 
 //	return pRTCArrayDate;
 }
+
+void Write_Byte_To_EEPROM(unsigned char Address, unsigned char DataByte)
+{
+	/*
+	// Function Purpose: Write_Byte_To_RTC writes a single byte on given address
+	// Address can have any value fromm 0 to 0xFF, and DataByte can have a value of 0 to 0xFF.
+	*/
+	I2C_Start();										// Start i2c communication
+
+	// Send i2c address of DS1307 with write command
+	while(I2C_Write_Byte(Device_Address_EEPROM + 0) == 1)// Wait until device is free
+	{	I2C_Start();	}
+
+	I2C_Write_Byte(Address);							// Write Address byte
+	I2C_Write_Byte(DataByte);							// Write data byte
+	I2C_Stop();											// Stop i2c communication
+}
+
+unsigned char Read_Byte_From_EEPROM(unsigned char Address)
+{
+	/*
+	Function Purpose: Read_Byte_From_RTC reads a single byte from given address
+	Address can have any value fromm 0 to 0xFF.
+	*/
+	unsigned char Byte = 0;								// Variable to store Received byte
+
+	I2C_Start();										// Start i2c communication
+
+	// Send i2c address of DS1307 with write command
+	while(I2C_Write_Byte(Device_Address_EEPROM + 0) == 1)// Wait until device is free
+	{	I2C_Start();	}
+
+	I2C_Write_Byte(Address);							// Write Address byte
+	I2C_ReStart();										// Restart i2c
+
+	// Send i2c address of DS1307 RTC with read command
+	I2C_Write_Byte(Device_Address_EEPROM + 1);
+
+	Byte = I2C_Read_Byte();								// Read byte from EEPROM
+
+	// Make SCK low, so that slave can stop driving SDA pin
+	// Send a NACK to indiacate single byte read is complete
+	I2C_Send_NACK();
+
+	// Send start bit and then stop bit to stop transmission
+	Set_SDA_Low;				// Make SDA Low
+	__delay_us(HalfBitDelay);	// Half bit delay
+	Set_SDA_High;				// Make SDA high
+	__delay_us(HalfBitDelay);	// Half bit delay
+
+	return Byte;				// Return the byte received from 24LC64 EEPROM
+}
+
+void Write_Bytes_To_EEPROM(unsigned char Address,unsigned char* pData,unsigned char NoOfBytes)
+{
+	/*
+	Function Purpose: Write_Bytes_To_RTC writes mulitple bytes from given starting address.
+	Address can have any value fromm 0 to 0xFF and pData is pointer to the array
+	containing NoOfBytes bytes in it. NoOfBytes is the number of bytes to write.
+	*/
+	unsigned int i;
+
+	I2C_Start();										// Start i2c communication
+
+	// Send i2c address of DS1307 with write command
+	while(I2C_Write_Byte(Device_Address_EEPROM + 0) == 1)// Wait until device is free
+	{	I2C_Start();	}
+
+	I2C_Write_Byte(Address);							// Write Address byte
+
+	for(i=0;i<NoOfBytes;i++)							// Write NoOfBytes
+		I2C_Write_Byte(pData[i]);						// Write data byte
+
+	I2C_Stop();											// Stop i2c communication
+}
+
+void Read_Bytes_From_EEPROM(unsigned char Address, unsigned char* pData, unsigned int NoOfBytes)
+{
+	/*
+ Function Purpose: Read_Bytes_From_RTC reads a NoOfBytes bytes from given starting address.
+ Address can have any value fromm 0 to 0xFF. NoOfBytes is the number of bytes to write.
+ Read bytes are returned in pData array.
+	*/
+	unsigned int i;
+
+	I2C_Start();										// Start i2c communication
+
+	// Send i2c address of DS1307 with write command
+	while(I2C_Write_Byte(Device_Address_EEPROM + 0) == 1)// Wait until device is free
+	{	I2C_Start();	}
+
+	I2C_Write_Byte(Address);							// Write Address byte
+	I2C_ReStart();										// Restart i2c
+
+	// Send i2c address of DS1307 RTC with read command
+	I2C_Write_Byte(Device_Address_EEPROM + 1);
+
+	pData[0] = I2C_Read_Byte();							// Read First byte from EEPROM
+
+	for(i=1;i<NoOfBytes;i++)							// Read NoOfBytes
+	{
+		I2C_Send_ACK();					// Give Ack to slave to start receiving next byte
+		pData[i] = I2C_Read_Byte();		// Read next byte from EEPROM
+	}
+
+	// Make SCK low, so that slave can stop driving SDA pin
+	// Send a NACK to indiacate read operation is complete
+	I2C_Send_NACK();
+
+	// Send start bit and then stop bit to stop transmission
+	Set_SDA_Low;				// Make SDA Low
+	__delay_us(HalfBitDelay);	// Half bit delay
+	Set_SDA_High;				// Make SDA high
+	__delay_us(HalfBitDelay);	// Half bit delay
+}
